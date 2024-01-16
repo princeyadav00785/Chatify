c87c8a82b3c98b52369b0c40b6d6dde49cf87d55
Prince yadav <princeyadav00785@gmail.com>
2024-01-16 12:43:20 +0530

    adding functionalities on homepgae

--- client/src/home.js
+++ client/src/home.js
@@ -1,6 +1,16 @@
 import styles from "./styles.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = ({ username, setUsername, room, setRoom, socket }) => {
+  const navigate = useNavigate();
+
+  const joinRoom = () => {
+    if (room !== "" && username !== "") {
+      socket.emit("join_room", { username, room });
+      navigate("/chat", { replace: true });
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
@@ -10,16 +20,21 @@ const Home = ({ username, setUsername, room, setRoom, socket }) => {
           placeholder="Username..."
           onChange={(e) => setUsername(e.target.value)}
         />
-        console.log({username});
-        <select className={styles.input}>
-          onChange={(e) => setRoom(e.target.value)}
+        <select
+          className={styles.input}
+          onChange={(e) => setRoom(e.target.value)} // Fix: Moved the onChange handler here
+        >
           <option>-- Select Room --</option>
           <option value="javascript">JavaScript</option>
           <option value="node">Node</option>
           <option value="express">Express</option>
           <option value="react">React</option>
         </select>
-        <button className="btn btn-secondary" style={{ width: "100%" }}>
+        <button
+          className="btn btn-secondary"
+          style={{ width: "100%" }}
+          onClick={joinRoom}
+        >
           Join Room
         </button>
       </div>
