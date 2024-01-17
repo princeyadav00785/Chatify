2216ba09732c1721ca5ea86d77cbb8dd08a2e253
Prince yadav <princeyadav00785@gmail.com>
2024-01-17 13:47:00 +0530

    adding leave chat option

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 const { MongoClient } = require("mongodb");
-require("dotenv").config(); // Load environment variables from .env file
+const mongoSaveMessage = require("./services/mongo-save-message"); // Change this line
+const mongoGetMessages = require("./services/mongo-get-message"); // Add this line
+const leaveRoom = require("./utils/leave-room");
 
 app.use(cors());
 
 const server = http.createServer(app);
-const CHAT_BOT = "ChatBot";
-const MongoSaveMessage = require("./services/mongo-save-message");
-const MongoGetMessage = require("./services/mongo-get-message");
 
 const io = new Server(server, {
   cors: {
@@ -20,13 +20,12 @@ const io = new Server(server, {
   },
 });
 
-// MongoDB connection URI and password from environment variables
-const mongoURI = process.env.MONGODB_URL;
-const mongoPwd = process.env.MONGODB_PWD;
+const CHAT_BOT = "ChatBot";
+let chatRoom = "";
+let allUsers = [];
 
-// Function to connect to MongoDB
 async function connectToMongo() {
-  const client = new MongoClient(`${mongoURI}/${mongoPwd}`, {
+  const client = new MongoClient(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
@@ -34,14 +33,13 @@ async function connectToMongo() {
   try {
     await client.connect();
     console.log("Connected to MongoDB");
-    return client.db(); // Return the database instance
+    return client.db();
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
   }
 }
 
-// Additional log statement for successful MongoDB connection
 connectToMongo()
   .then(() => {
     console.log("Server successfully connected to MongoDB");
@@ -55,54 +53,66 @@ io.on("connection", (socket) => {
 
   socket.on("join_room", async (data) => {
     const { username, room } = data;
-
-    // Connect to MongoDB
-    const db = await connectToMongo();
-
-    // Use the correct function for getting messages (MongoGetMessage instead of harperGetMessages)
-    MongoGetMessage(room)
-      .then((last100Messages) => {
-        socket.emit("last_100_messages", last100Messages);
-      })
-      .catch((err) => console.log(err));
-
     socket.join(room);
 
     let __createdtime__ = Date.now();
-
-    // Emit welcome message
     socket.to(room).emit("receive_message", {
       message: `${username} has joined the chat room`,
       username: CHAT_BOT,
       __createdtime__,
     });
-
     socket.emit("receive_message", {
       message: `Welcome ${username}`,
       username: CHAT_BOT,
       __createdtime__,
     });
 
-    socket.on("disconnect", async () => {
-      console.log("User disconnected");
+    chatRoom = room;
+    allUsers.push({ id: socket.id, username, room });
+    chatRoomUsers = allUsers.filter((user) => user.room === room);
+    socket.to(room).emit("chatroom_users", chatRoomUsers);
+    socket.emit("chatroom_users", chatRoomUsers);
 
-      // Emit user leaving message
-      socket.to(room).emit("receive_message", {
-        message: `${username} has left the chat room`,
-        username: CHAT_BOT,
-        __createdtime__,
-      });
+    mongoGetMessages(room)
+      .then((last100Messages) => {
+        socket.emit("last_100_messages", last100Messages);
+      })
+      .catch((err) => console.log(err));
+  });
 
-      // Disconnect from MongoDB
-      db.close();
+  socket.on("send_message", (data) => {
+    const { message, username, room, __createdtime__ } = data;
+    io.in(room).emit("receive_message", data);
+    mongoSaveMessage(message, username, room, __createdtime__) // Change this line
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  });
+
+  socket.on("leave_room", (data) => {
+    const { username, room } = data;
+    socket.leave(room);
+    const __createdtime__ = Date.now();
+    allUsers = leaveRoom(socket.id, allUsers);
+    socket.to(room).emit("chatroom_users", allUsers);
+    socket.to(room).emit("receive_message", {
+      username: CHAT_BOT,
+      message: `${username} has left the chat`,
+      __createdtime__,
     });
+    console.log(`${username} has left the chat`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected from the chat");
+    const user = allUsers.find((user) => user.id == socket.id);
+    if (user?.username) {
+      allUsers = leaveRoom(socket.id, allUsers);
+      socket.to(chatRoom).emit("chatroom_users", allUsers);
+      socket.to(chatRoom).emit("receive_message", {
+        message: `${user.username} has disconnected from the chat.`,
+      });
+    }
   });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
-});
+server.listen(4000, () => "Server is running on port 4000");
